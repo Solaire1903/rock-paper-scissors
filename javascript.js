0dca1c383baaae9edff7a71967734bc6d22acb8b
Solaire1903 <marvkie@gmail.com>
2025-06-04 19:43:55 +0200

    Add computer choice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("Test");
+function getComputerChoice() {
+    let choice = Math.random();
+
+    if (choice <= 0.33) {
+        return "rock";
+    }
+    else if (choice <= 0.66) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
