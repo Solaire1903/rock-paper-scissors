e09e3d397e6ad5699dddd568ef0bc4d00c0a6542
Solaire1903 <marvkie@gmail.com>
2025-06-04 20:14:52 +0200

    Add play round function

--- javascript.js
+++ javascript.js
@@ -22,5 +22,44 @@ function getHumanChoice() {
     return choice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    }    
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Rock beats Scissors");
+            humanScore++;
+        }
+    }
+
+    if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock");
+            humanScore++;
+        }
+        else {
+            console.log("You lose! Scissors beats Paper");
+            computerScore++;
+        }
+    }
+    
+    if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors");
+            computerScore++;
+        }
+        else {
+            console.log("You win! Scissors beats Paper");
+            humanScore++;
+        }
+    }
+}
+
 let humanScore = 0;
 let computerScore = 0;
